c2f7320e2ae597896417a600a607959d035e6188
trileesha <trileeshathaver@gmail.com>
2023-04-21 14:48:28 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -18,5 +18,5 @@ printf("Size of an int: %lu byte(S)\n", (unsigned long)sizeof(b));
 printf("Size of a long int: %lu byte(S)\n", (unsigned long)sizeof(c));
 printf("Size of a long long int: %lu byte(S)\n", (unsigned long)sizeof(d));
 printf("Size of a float: %lu byte(S)\n", (unsigned long)sizeof(f));
-return (0);
+:return (0);
 }
